6de2bafe9be91c2a50779a931031e7938914afa4
MypkaXD <thuwashev2016@ayndex.ru>
2022-11-05 23:20:21 +0300

    add 3 tasks

--- praktika code war.cpp
+++ praktika code war.cpp
@@ -1,11 +1,52 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
 #include "conver_string_to_int.h"
 #include "Convert boolean values to strings 'Yes' or 'No'.h"
 #include "Descending Order.h"
 #include "Vowel Count.h"
 #include "Complementary DNA.h"
+#include "You only need one - Beginner.h"
+#include "Isograms.h"
+#include "add_binary.h"
+
+/*
+int main() {
+	std::cout << add_binary(1, 1);
+	return 0;
+}
+*/
+
+/*
+int main() {
+	std::string temp1 = "moose";
+	if (is_isogram(temp1))
+		std::cout << "TRUE";
+	else std::cout << "FALSE";
+
+	return 0;
+}
+*/
+
+/*
+int main() {
+	std::vector<std::string> seq = { "a","b", "C" };
+	std::string elem = "C";
+	std::string elem2 = "A";
+
+	if (check(seq, elem))
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+
+	if (check(seq, elem2))
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+	return 0;
+}
+*/
 
 /*
 int main() {
